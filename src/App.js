07a802e923be633954981d7b1484c90ed4af6cc6
madhur108 <madhur.garg.che19@iitbhu.ac.in>
2021-01-14 11:52:25 +0530

    simple weather_App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import logo from './logo.svg';
+// import './App.css';
+import React, { Component } from "react";
+import Show from "./Show";
+const api = {
+  key: "75b4b771dd5e7ecec7efad87a1d2bba3",
+  base: "https://api.openweathermap.org/data/2.5/"
 }
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {     //state stores dynamic data
+      temperature: "",
+      query: "",
+      humidity: "",
+      wind_speed: "",
+      visibility: "",
+      weather: ""
+    };
+    this.search = this.search.bind(this);    //Jo render ke inside hai only usi function ko bind karna pdega
+  }
+
+  // addItem(e){
+  //   if(this._inputElement !== ""){
+
+  //   }
+
+
+  search(e) {
+    // console.log(2+3);
+    fetch(`${api.base}weather?q=${this.city.value}&units=metric&appid=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        console.log(result);
+        if (typeof(result.main) !== "undefined") {
+          this.setState({
+            humidity: result.main.humidity,
+            wind_speed: result.wind.speed,
+            visibility: result.visibility,
+            weather: result.weather[0].description,
+            temperature: result.main.temp,
+            query: this.city.value
+
+          });
+        }
+        else {
+          this.setState({
+            query: "",
+            temperature: result.message
+
+          });
+        }
+        
+      });
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div id="container">
+        {/* <button onClick={this.search}>Get Temp</button> */}
+        <form onSubmit={this.search}>
+          <input id="input" ref={(a) => this.city = a} placeholder="Enter City Name">
+          </input>
+          {/* <button id="button" type="submit"></button> */}
+          <i id="icon" type="submit" class="fas fa-search" onClick={this.search}></i>
+          
+        </form>
+        <Show query={this.state.query} temperature={this.state.temperature} weather={this.state.weather} visibility={this.state.visibility} wind_speed={this.state.wind_speed} humidity={this.state.humidity} />
+      </div>
+    );
+
+  }
+
+};
+
 export default App;
+
---
+++ src/Show.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react"
+class Show extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        let queries = this.props.query;
+        let temperature = this.props.temperature;
+        let weather = this.props.weather;
+        let wind_speed = this.props.wind_speed;
+        let visibility = this.props.visibility;
+        let humidity = this.props.humidity;
+        if (queries !== "") {
+            return (
+                <div id="main">
+                    <center>
+                        <div id="main2">
+                            <div>
+                            <h3>Temperature : {temperature}<sup>0</sup>C</h3>
+                            <h3>Weather :     {weather}</h3>
+                            <h3>Wind_speed : {wind_speed}</h3>
+                            <h3>Visibility : {visibility}</h3>
+                            <h3>Humidity : {humidity}</h3>
+                            </div>
+                            
+                            {/* <h3>Temperature:{temperature}<sup>0</sup>C</h3>
+                            <h3>Weather:  {weather}</h3>
+                            <h3>Wind_speed:  {wind_speed}</h3>
+                            <h3>Visibility:  {visibility}</h3>
+                            <h3>Humidity:{humidity}</h3> */}
+                        </div>
+                    </center>
+                </div>
+            )
+        }
+        else {
+            return (
+                <div>
+                    <center>
+                        <h2>{temperature}</h2>
+                        {/* <h3>ughlg</h3> */}
+                    </center>
+                </div>
+            )
+        }
+
+    }
+}
+
+export default Show;
